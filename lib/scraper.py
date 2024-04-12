d89109df4cd24bb2043266ae00bb42ac15df4428
Immanuel <immanuelcherui1@gmail.com>
2024-04-12 12:21:29 +0300

    Done with web scrapping

--- lib/scraper.py
+++ lib/scraper.py
@@ -3,4 +3,12 @@ from bs4 import BeautifulSoup
 import requests
 
 headers = {'user-agent': 'my-app/0.0.1'}
-html = requests.get("https://flatironschool.com/", headers=headers)
+
+# We will be uses the our-courses page
+html = requests.get("https://flatironschool.com/our-courses/", headers=headers)
+doc = BeautifulSoup(html.text, 'html.parser')
+
+courses = doc.select('.heading-25-extrabold.color-black')
+
+for course in courses:
+  print(course.contents[0].strip())
